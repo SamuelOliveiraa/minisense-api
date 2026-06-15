d7a1fcad13b7053d825f30b0d3325729d5050aaf
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2026-06-15 00:16:06 -0300

    chore: Update file server.ts

--- src/server.ts
+++ src/server.ts
@@ -14,12 +14,4 @@ async function startServer() {
   }
 }
 
-// Vercel handler
-export default async function handler(req: any, res: any) {
-  await app.ready();
-  app.server.emit("request", req, res);
-}
-
-if (env.NODE_ENV !== "production") {
-  startServer();
-}
+startServer();
